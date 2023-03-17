16d1bc60a14dd5a8f3f3ff717606e93b7d93ffba
paulomarcelonds <paulomarcelonds@gmail.com>
2023-03-17 13:39:39 -0300

    exemplos de tipos primitivos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        byte idade = 35;
+        short ano = 1987;
+        int cep = 21070333;
+        long cpf = 98765432109l;
+        float pi = 3.14f;
+        double salario = 1500.50;
+
+
+        System.out.println(idade);
+        System.out.println(ano);
+        System.out.println(cep);
+        System.out.println(cpf);
+        System.out.println(pi);
+        System.out.println(salario);
     }
 }
